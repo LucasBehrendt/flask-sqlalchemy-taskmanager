cbe169b53aa75a000521fb387e2db41f3710b7d7
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-05-08 14:42:59 +0000

    create task posting to database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -7,3 +7,14 @@ $("#copyright").text(new Date().getFullYear());
 $(document).ready(function () {
   $('.modal').modal();
 });
+
+$(document).ready(function(){
+  $('.datepicker').datepicker({
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+  });
+});
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
